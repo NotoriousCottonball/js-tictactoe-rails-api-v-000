7dbaea6a9041338dca0427da674189ca854daee1
George  Pianka <gpianka1@gmail.com>
2019-05-11 08:24:16 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -28,8 +28,8 @@ $(document).ready(attachListeners);
 let gameNumber = 0
 
  function player() {
-  return (turn % 2 == 0 ? "X" : "O");
-};
+  return (turn % 2 === 0 ? "X" : "O");
+}
 
  function updateState(choice) {
   $(choice).text(player());
